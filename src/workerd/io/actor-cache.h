e3f71db23b1b543a21b0f3a99917476a2edc5940
Ben Caimano <bcaimano@cloudflare.com>
2023-09-07 14:57:49 -0400

    Refactor: Use coroutines with actor cache "backpressure"
    This bit was fairly easy to convert and we can also what we are doing with "backpressure" more obvious in the process.

--- src/workerd/io/actor-cache.h
+++ src/workerd/io/actor-cache.h
@@ -725,7 +725,9 @@ private:
 
   // If the LRU is currently over the soft limit, returns a promise that resolves when it is
   // back under the limit.
-  kj::Maybe<kj::Promise<void>> getBackpressure();
+  kj::Maybe<kj::Promise<void>> flushIfNeeded();
+  kj::Promise<void> flushWhileOverDirtyLimit();
+  bool checkIfFlushNeeded();
 
   class GetMultiStreamImpl;
   class ForwardListStreamImpl;
